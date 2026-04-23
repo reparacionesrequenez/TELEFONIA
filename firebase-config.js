6d994db81184478647405c940bdd9d9629ba8552
reparacionesrequenez <martinreq@gmail.com>
2026-04-23 16:01:55 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 const firebaseConfig = {
   apiKey: "AIzaSyAlcMzRwy4G2uc3CeKXvAj3HZdm1xGYjI0",
   authDomain: "telefonia-d8f6d.firebaseapp.com",
+  databaseURL: "https://telefonia-d8f6d-default-rtdb.firebaseio.com",
   projectId: "telefonia-d8f6d",
   storageBucket: "telefonia-d8f6d.firebasestorage.app",
   messagingSenderId: "842673328033",
-  appId: "1:842673328033:web:5a018f68e50ccc6cca85e0",
-  measurementId: "G-0V7GFZY49H"
+  appId: "1:842673328033:web:854243bd09dadd45ca85e0",
+  measurementId: "G-0SZ1FLVVDG"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
